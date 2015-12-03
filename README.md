affe1b827076ac6c3df28d7b16a8808f6bece615
FroggiesareFluffy <jhia186034@adams12.org>
2015-12-02 18:24:21 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ForestOfSquirrels
+# Forest Of Squirrels
+This is a game with squirrels in it. More details will be added when we figure them out.
+
+## Instructions
+
+Arrow keys to hop.
+
+## Dependencies
+
++ Python (Tested with 3.5, but should work for other versions)
++ Pygame
+
+## The Team
+
+Current members:
+
+![John Hiatt](https://github.com/FroggiesareFluffy/ForestOfSquirrels/blob/development/forestofsquirrels/graphics/squirrel.png?raw=true)John Hiatt (FroggiesAreFluffy)
--- forestofsquirrels/squirrels/seller.py
+++ forestofsquirrels/squirrels/seller.py
@@ -7,7 +7,7 @@ class Seller(Squirrel):
         Squirrel.update(self)
         for squirrel in self.forest.sprites():
             if isinstance(squirrel, Squirrel) and squirrel != self and math.sqrt(
-                                    (self.x - squirrel.x) ** 2 + (self.y - squirrel.y) ** 2) < 50:
+                                    (self.x - squirrel.x) ** 2 + (self.y - squirrel.y) ** 2) < 150:
                 if self.x > squirrel.x:
                     self.image = self.leftimg
                 else:
