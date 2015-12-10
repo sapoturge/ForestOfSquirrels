d8eb0d09f1e001ee25fb97bf983388c891c49646
FroggiesareFluffy <jhia186034@adams12.org>
2015-12-09 19:35:08 -0700

    Area is generated

--- forestofsquirrels/world/forest.py
+++ forestofsquirrels/world/forest.py
@@ -6,10 +6,11 @@ import forestofsquirrels.trees
 
 class Area(pygame.sprite.Sprite, pygame.sprite.AbstractGroup):
     def __init__(self, forest, x, y, area_name):
-        self.x = x
-        self.y = y
         self.forest = forest
         self.connections = {}
+        self.realx = x
+        self.realy = y
+        self.forest.areas[x, y] = self
         pygame.sprite.Sprite.__init__(self, forest)
         pygame.sprite.AbstractGroup.__init__(self)
         with open("forestofsquirrels/world/areas/{}.area".format(area_name)) as area:
@@ -24,34 +25,49 @@ class Area(pygame.sprite.Sprite, pygame.sprite.AbstractGroup):
                 elif line == "CONNECTIONS:":
                     mode = "connection"
                 elif mode == "tree":
-                    tree, x, y = line.split(",")
-                    forestofsquirrels.trees.Tree.create_tree(self, int(x), int(y), tree)
+                    tree, tx, ty = line.split(",")
+                    forestofsquirrels.trees.Tree.create_tree(self, int(tx), int(ty), tree)
                 elif mode == "connection":
                     side, area = line.split(",")
                     self.connections[side] = area
+        self.x = x * self.width
+        self.y = y * self.height
         for s in self.sprites():
-            s.x -= self.x
-            s.y -= self.y
+            s.x += self.x
+            s.y += self.y
             if isinstance(s, forestofsquirrels.squirrels.Player):
                 self.remove_internal(s)
             forest.add_internal(s)
             s.add_internal(forest)
         self.dead = False
         self.image = pygame.Surface((0, 0))
-        self.forest.player = self.player
-        self.player.forest = self.forest
+        if hasattr(self, "player"):
+            self.forest.player = self.player
+            self.player.forest = self.forest
 
     def update(self):
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        if not self.rect.colliderect(self.forest.screenrect.inflate(1280, 960)):
+        bigrect = self.forest.screenrect.inflate(1280, 960)
+        if not (self.rect.colliderect(bigrect) or bigrect.contains(self.rect)):
             if not self.dead:
                 for s in self.sprites():
                     self.forest.remove(s)
                 self.dead = True
+                print("Dead at {}, {} ({}, {})!".format(self.x, self.y, self.realx, self.realy))
+                print(bigrect)
         elif self.dead:
             for s in self.sprites():
                 self.forest.add(s)
             self.dead = False
+        if bigrect.contains(self.rect):
+            if (self.realx, self.realy + 1) not in self.forest.areas:
+                Area(self.forest, self.realx, self.realy + 1, "forest").update()
+            elif (self.realx, self.realy - 1) not in self.forest.areas:
+                Area(self.forest, self.realx, self.realy - 1, "forest").update()
+            elif (self.realx + 1, self.realy) not in self.forest.areas:
+                Area(self.forest, self.realx + 1, self.realy, "forest").update()
+            elif (self.realx - 1, self.realy) not in self.forest.areas:
+                Area(self.forest, self.realx - 1, self.realy, "forest").update()
 
     def add_internal(self, other):
         if isinstance(other, pygame.sprite.AbstractGroup):
@@ -70,14 +86,13 @@ class Forest(pygame.sprite.LayeredUpdates):
     def __init__(self, camera_x=0, camera_y=0):
         self.camera_x = camera_x
         self.camera_y = camera_y
+        self.areas = {}
         self.screenrect = pygame.Rect(camera_x, camera_y, 640, 480)
         pygame.sprite.LayeredUpdates.__init__(self)
 
     def update(self):
         for s in self.sprites():
             s.update()
-        # self.camera_x = min(max(self.camera_x, 0), self.width - 640)
-        # self.camera_y = min(max(self.camera_y, 0), self.height - 480)
         for s in self.sprites():
             s.rect.x -= self.camera_x
             s.rect.y -= self.camera_y
