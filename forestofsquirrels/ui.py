6a348f760e5a83827a78e9a79a8cff04a913778e
FroggiesareFluffy <jhia186034@adams12.org>
2015-12-12 11:21:44 -0700

    Added chat bar.

--- forestofsquirrels/ui.py
+++ forestofsquirrels/ui.py
@@ -13,36 +13,63 @@ class DisplayBar(UIElement):
     def __init__(self, group, squirrel):
         UIElement.__init__(self, group, squirrel)
         self.level = 1
-
-    @property
-    def image(self):
         self.rect = pygame.Rect(0, 480 - 20, 640, 20)
-        image = pygame.Surface(self.rect.size)
-        image.fill((127, 127, 127))
-        return image
+        self.image = pygame.Surface(self.rect.size)
+        self.image.fill((127, 127, 127))
 
 
 class HealthBar(UIElement):
-    @property
-    def image(self):
-        image = pygame.Surface((self.squirrel.health * 16, 16)).convert_alpha()
-        image.fill((0, 0, 0, 0))
+    def update(self):
+        self.image = pygame.Surface((self.squirrel.health * 18, 18)).convert_alpha()
+        self.image.fill((0, 0, 0, 0))
         for y in range(self.squirrel.health):
-            pygame.draw.circle(image, (255, 0, 0), (y * 16 + 8, 8), 8)
-        self.rect = image.get_rect()
+            pygame.draw.circle(self.image, (255, 0, 0), (y * 18 + 9, 9), 8)
+        self.rect = self.image.get_rect()
         self.rect.bottomright = (640, 480)
-        return image
 
 
 class AcornIndicator(UIElement):
-    @property
-    def image(self):
+    def update(self):
         if self.squirrel.acorn:
-            image = pygame.image.load("forestofsquirrels/graphics/acorn.png").convert_alpha()
-            self.rect = image.get_rect()
+            self.image = pygame.image.load("forestofsquirrels/graphics/acorn.png").convert_alpha()
+            self.rect = self.image.get_rect()
             self.rect.bottom = 480
-            return image
-        return pygame.Surface((0, 0))
+        else:
+            self.image = pygame.Surface((0, 0))
+
+
+class ChatBox(UIElement):
+    font = pygame.font.Font("freesansbold.ttf", 18)
+
+    def __init__(self, group, squirrel):
+        UIElement.__init__(self, group, squirrel)
+        self.rect = pygame.Rect(20, 460, 640 - 20 - 8 * 18, 20)
+        self.selected = False
+        self.text = ""
+        self.image = pygame.Surface(self.rect.size)
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if self.rect.collidepoint(event.pos):
+                    self.selected = True
+                else:
+                    self.selected = False
+                    pygame.event.post(event)
+            elif event.type == pygame.KEYDOWN and self.selected:
+                if event.key == pygame.K_RETURN:
+                    self.squirrel.say(self.text)
+                elif event.key == pygame.K_BACKSPACE and len(self.text) > 0:
+                    self.text = self.text[:-1]
+                elif event.unicode and event.unicode.lower() in "abcdefghijklmnopqrstuvwxyz .?!1234567890,":
+                    self.text += event.unicode
+                else:
+                    pygame.event.post(event)
+            else:
+                pygame.event.post(event)
+        self.image.fill((0, 0, 0))
+        self.image.blit(self.font.render(self.text, False, (0, 0, 0), (255, 255, 255)), (1, 1))
+        return self.image
 
 
 def create_ui(forest):
@@ -51,9 +78,11 @@ def create_ui(forest):
     DisplayBar(HUD, forest.player)
     HealthBar(HUD, forest.player)
     AcornIndicator(HUD, forest.player)
+    ChatBox(HUD, forest.player)
 
 
 def update():
+    HUD.update()
     HUD.draw(pygame.display.get_surface())
     update.update()
 
